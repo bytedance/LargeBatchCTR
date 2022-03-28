455de6e88c5b86c57254d1ba2ed889a28127e337
zangwei.zheng <zhengzangwei@bytedance.com>
2022-03-28 19:44:44 +0800

    add basic training

--- train.py
+++ train.py
@@ -4,14 +4,11 @@ from math import sqrt
 import numpy as np
 import tensorflow as tf
 import tensorflow_addons as tfa
-from sklearn.model_selection import train_test_split
-from tqdm import tqdm
 
 from deepctr.feature_column import DenseFeat, SparseFeat, get_feature_names
 from deepctr.models import DeepFM
-
-from .utils import create_logdir, print_curtime, tf_allow_growth
-from .data_utils import load_data, load_feature_name, auc
+from data_utils import load_data, load_feature_name
+from utils import create_logdir, print_curtime, tf_allow_growth, auc
 
 
 def parseargs():
@@ -23,25 +20,25 @@ def parseargs():
 
     # Debug
     parser.add_argument("--eager", action="store_true")
-    parser.add_argument("--log_freq", default=0, type=int)
+    parser.add_argument("--log", action="store_true")
+    parser.add_argument("--log_freq", default=100, type=int)
 
     # Hyperparemters
     parser.add_argument("--epoch", default=10, type=int)
     parser.add_argument("--opt", type=str, default="adam")
     parser.add_argument("--sparse_embed_dim", type=int, default=4)
     parser.add_argument("--dropout", type=float, default=0)
-    parser.add_argument("--l2", type=float, default=1e-5)
 
     # HPs
     parser.add_argument("--bs", default=1024, type=int)
     parser.add_argument("--lr", default=1e-4, type=float)
     parser.add_argument("--lr_embed", default=1e-4, type=float)
-    parser.add_argument("--l2_embed", type=float, default=1e-5)
+    parser.add_argument("--l2", type=float, default=1e-5)
 
     # HPs
     parser.add_argument("--clip", type=float, default=0)
     parser.add_argument("--warmup", type=float, default=0)
-    parser.add_argument("--init_stddev", type=float, default=1e-2)
+    parser.add_argument("--init_stddev", type=float, default=1e-4)
 
     args = parser.parse_args()
     return args
@@ -68,7 +65,98 @@ def get_feature_column(data, sparse_features, dense_features):
 
 
 class CustomModel(tf.keras.Model):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.cur_step = tf.Variable(0, trainable=False, dtype=tf.int64)
+
+    def train_step(self, data):
+        ret = dict()
+
+        # log setting
+        self.cur_step.assign_add(1)
+
+        def should_record(): return tf.equal(
+            tf.math.floormod(self.cur_step, args.log_freq), 0
+        )
+        record_env = tf.summary.record_if(should_record)
+        tf.summary.experimental.set_step(self.cur_step)
+
+        # assist vars
+        name_to_layer = {x.name: x for x in self.trainable_variables}
+        uniq_ids, uniq_cnt = dict(), dict()
+        for k, v in data[0].items():
+            if k[0] != "I":
+                y, _, count = tf.unique_with_counts(v)
+                uniq_ids[k] = y
+                uniq_cnt[k] = count
+
+        # main
+        x, y = data
+        with tf.GradientTape() as tape:
+            y_pred = self(x, training=True)
+            loss = self.compiled_loss(y, y_pred)
+            # loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses)
+
+        trainable_vars = self.trainable_variables
+        gradients = tape.gradient(loss, trainable_vars)
+        name_to_gradient = {
+            x.name: g for x, g in zip(self.trainable_variables, gradients)
+        }
+        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+
+        # =====
+        # Logging
+        # =====
+        embed_index = [
+            i for i, x in enumerate(trainable_vars) if "embeddings" in x.name
+        ]
+        dense_index = [i for i in range(
+            len(trainable_vars)) if i not in embed_index]
+        embed_vars = [trainable_vars[i] for i in embed_index]
+        dense_vars = [trainable_vars[i] for i in dense_index]
+        embed_gradients = [gradients[i] for i in embed_index]
+        dense_gradients = [gradients[i] for i in dense_index]
+
+        with record_env:
+            tf.summary.scalar("loss/loss", loss)
+            tf.summary.scalar("global_norm/global",
+                              tf.linalg.global_norm(gradients))
+            tf.summary.scalar("global_norm/dense",
+                              tf.linalg.global_norm(dense_gradients))
+            tf.summary.scalar("global_norm/embed",
+                              tf.linalg.global_norm(embed_vars))
+            tf.summary.scalar("global_norm/var_dense",
+                              tf.linalg.global_norm(dense_vars))
+            tf.summary.scalar("global_norm/var_embed",
+                              tf.linalg.global_norm(embed_gradients))
+
+            if args.log:
+                for i, (variable, gradient) in enumerate(
+                    zip(trainable_vars, gradients)
+                ):
+                    name = variable.name
+                    opt_index = 0 if i in embed_index else 1
+                    m = self.optimizer.optimizer_specs[opt_index]["optimizer"].get_slot(
+                        variable, "m"
+                    )
+                    v = self.optimizer.optimizer_specs[opt_index]["optimizer"].get_slot(
+                        variable, "v"
+                    )
+
+                    layer_norm = tf.norm(variable)
+                    grad_norm = tf.norm(gradient)
+                    m_norm = tf.norm(m)
+                    v_norm = tf.norm(v)
+
+                    tf.summary.scalar("layer_norm/" + name, layer_norm)
+                    tf.summary.scalar("grad_norm/" + name, grad_norm)
+                    tf.summary.scalar("m_norm/" + name, m_norm)
+                    tf.summary.scalar("v_norm/" + name, v_norm)
+
+        self.compiled_metrics.update_state(y, y_pred)
+        for m in self.metrics:
+            ret[m.name] = m.result()
+        return ret
 
 
 if __name__ == "__main__":
@@ -101,9 +189,9 @@ if __name__ == "__main__":
         model_args = dict(
             dnn_hidden_units=(400, 400, 400),
             dnn_dropout=args.dropout,
-            l2_reg_linear=0,
-            l2_reg_embedding=0,
-            custom_model=CustomModel,
+            l2_reg_linear=args.l2,
+            l2_reg_embedding=args.l2,
+            keras_model=CustomModel,
         )
         if args.model == "DeepFM":
             model_class = DeepFM
